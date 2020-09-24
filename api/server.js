0114c6c3f4ec2ad11c9646e5d8edac341d19e06a
Shomari Roberts <shomariroberts@gmail.com>
2020-09-24 01:17:09 -0400

    initial setup

---
+++ api/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(logger);
+server.use(express.json());
+
+//Will add Router when completed
+
+server.get('/', (req, res) => {
+  res.status(200).json({
+    api: 'up',
+  });
+});
+
+function logger(req, res, next) {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get(
+      'host'
+    )}`
+  );
+
+  next();
+}
+
+module.exports = server;
